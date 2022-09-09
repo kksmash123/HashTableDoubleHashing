2e7040aae86b4e7c0e9aa045e6b51d1e0649dc95
kksmash123 <kksmashes@gmail.com>
2022-09-09 14:41:47 +0530

    Add project files.

---
+++ HashTableDoubleHashing/HashTableDoubleHashing.cpp
@@ -0,0 +1,69 @@
+// HashTableDoubleHashing.cpp 
+
+
+
+#include <iostream>
+#define sh 10
+#define p 7
+
+using namespace std;
+
+
+int h1(int key)
+{
+	return key % sh;
+}
+
+int h2(int key)
+{
+	return p - (key % p);
+}
+
+
+int DoubleHashing(int ht[],int i,int key)
+{
+	int vh1 = i;
+	int vh2 = h2(key);
+	int k = 1;
+
+	while (ht[(vh1 + (k * vh2)) % sh] != 0)
+		k++;
+
+	return (vh1 + (k * vh2)) % sh;
+
+}
+
+void insert(int ht[], int key)
+{
+	int i = h1(key);
+
+	if (ht[i] != 0)
+	{
+		i = DoubleHashing(ht, i, key);
+	}
+
+	ht[i] = key;
+}
+
+
+
+
+int main()
+{
+
+	//hashtable 
+	int ht[10] = { 0 };
+
+
+	int a[] = { 5,25,15,35,95 };
+	int n = sizeof(a) / sizeof(a[0]);
+
+	//insert into the hash table
+	for (int i = 0;i < n;i++)
+		insert(ht, a[i]);
+
+
+
+
+
+}
