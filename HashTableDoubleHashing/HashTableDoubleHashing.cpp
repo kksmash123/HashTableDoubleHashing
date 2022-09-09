cb3c999023a5ee8caa7d1086dd9b3180aea0009d
kksmash123 <kksmashes@gmail.com>
2022-09-09 16:06:00 +0530

    successful and unsuccessful search in hashtable using double hashing

--- HashTableDoubleHashing/HashTableDoubleHashing.cpp
+++ HashTableDoubleHashing/HashTableDoubleHashing.cpp
@@ -8,6 +8,13 @@
 
 using namespace std;
 
+int h1(int key);
+int h2(int key);
+int DoubleHashing(int ht[], int i, int key);
+void insert(int ht[], int key);
+
+
+
 
 int h1(int key)
 {
@@ -18,7 +25,6 @@ int h2(int key)
 {
 	return p - (key % p);
 }
-
 
 int DoubleHashing(int ht[],int i,int key)
 {
@@ -45,7 +51,22 @@ void insert(int ht[], int key)
 	ht[i] = key;
 }
 
+int search(int ht[], int key)
+{
 
+	int h1i = h1(key);
+	int h2i = h2(key);
+
+	int k = 0;
+	while (ht[(h1i+(k*h2i))%sh] != key)
+	{
+		k++;
+		if (ht[(h1i + (k * h2i)) % sh] == 0)
+			return -1;
+
+	}
+	return (h1i + (k * h2i)) % sh;
+}
 
 
 int main()
@@ -62,6 +83,11 @@ int main()
 	for (int i = 0;i < n;i++)
 		insert(ht, a[i]);
 
+	//search in the hashtable
+	search(ht, 95);
+
+	//unsuccessful search
+	search(ht, 105);
 
 
 
